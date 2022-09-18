72973fb824fafd769e67460dd654dd769e83af62
GregLtrnr <gregory.letourneur@epitech.eu>
2022-09-18 21:59:07 +0200

    add permissions for jail cast

--- src/main/java/com/pilzo/magicspell/EventListeners.java
+++ src/main/java/com/pilzo/magicspell/EventListeners.java
@@ -1,12 +1,12 @@
 package com.pilzo.magicspell;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.pilzo.magicspell.spells.Jail;
@@ -18,8 +18,15 @@ public class EventListeners implements Listener {
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
           if(event.getItem().getType() == Material.BLAZE_ROD){
             Player player = event.getPlayer();
-            Block block = player.getTargetBlock(null, 10);
-            Jail jail = new Jail(block,player);
+            if(player.hasPermission("magicspell.cast.jail")){
+              Block block = player.getTargetBlock(null, 10);
+              Jail jail = new Jail(block,player);
+            }else{
+              //send a red message to the player
+              player.sendMessage(ChatColor.RED+"You don't have permission to cast this spell.");
+
+
+            }
           }
           // if matirial is wooden stick, print the target block informations
           if(event.getItem().getType() == Material.STICK){
